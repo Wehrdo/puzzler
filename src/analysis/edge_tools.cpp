8c7b44a32a643e5e7932b5b8ac9af8c958d60080
David Wehr <dawehr@iastate.edu>
2017-04-08 15:30:51 -0500

    Fixed drawing issues. memory clearing

--- src/analysis/edge_tools.cpp
+++ src/analysis/edge_tools.cpp
@@ -17,7 +17,7 @@ cv::Mat draw_curve(const std::vector<Vec2d>& points, int width, std::vector<Vec2
                 return cv::Point((pt.x - min_x.x) * scale,
                                 scale * (max_y.y - pt.y));};
     // Create image matrix with dimensions to fill width, and proportional height
-    cv::Mat out_img(height, width, CV_8UC1);
+    cv::Mat out_img(height, width, CV_8UC1, cv::Scalar(0));
     cv::Scalar color = cv::Scalar(255, 255, 255);
 
 
@@ -36,3 +36,45 @@ cv::Mat draw_curve(const std::vector<Vec2d>& points, int width, std::vector<Vec2
 
     return out_img;
 }
+
+std::vector<std::size_t> find_inflections(std::vector<Vec2d > points, double threshold) {
+    std::size_t N = points.size();
+    if (N < 3) {
+        return std::vector<std::size_t>();
+    }
+    double first_angle = calc_angle(Vec2d(1,0), points[0] - points[N-1]);
+    double second_angle = calc_angle(Vec2d(1,0), points[1] - points[0]);
+    printf("raw: %f\nraw: %f\n", first_angle, second_angle);
+    int last_sign = sign(second_angle - first_angle);
+
+    double last_angle = second_angle;
+    std::vector<std::size_t> inflections;
+    for (std::size_t i = 0; i < N; ++i) {
+        // printf("x: %f, y: %f\n", points[i].x, points[i].y);
+        Vec2d new_vec = points[i] - points[i-1];
+        // double angle = calc_angle(new_vec, last_vec);
+        double raw_angle = calc_angle(Vec2d(1,0), new_vec);
+        double angle_diff = raw_angle - last_angle;
+        int this_sign = sign(angle_diff);
+        if (this_sign != last_sign) {
+            printf("Turning point at %f\n", points[i].x);
+            inflections.push_back(i);
+        }
+        printf("raw: %f, diff: %f\n", raw_angle, angle_diff);
+        last_angle = raw_angle;
+        last_sign = this_sign;
+    }
+    return inflections;
+}
+
+// std::vector<Vec2d> calc_curvature(const std::vector<Vec2d>& points) {
+//     std::vector<Vec2d> curve();
+//     std::vector<double> distances;
+//     curve.push_back((points[0] - points[N-1]).normalized());
+    
+//     for (std::size_t i = 1; i < points.size(); i++) {
+//         curve.push_back((points[i] - points[i-1]).normalized());
+//     }
+//     // Curve now contains vectors for all lines
+
+// }
--- src/analysis/edge_tools.hpp
+++ src/analysis/edge_tools.hpp
@@ -34,7 +34,16 @@ public:
     T operator*(const Vec2<T>& other) {
         return (this->x * other.x) + (this->y * other.y);
     };
+    // Scalar multiply
+    T operator*(const T factor) {
+        return Vec2<T>(this->x * factor, this->y * factor);
+    }
+    // Scalar divide
+    T operator/(const T factor) {
+        return Vec2<T>(this->x / factor, this->y / factor);
+    }
     T norm();
+    Vec2<T> normalized();
 };
 
 
@@ -42,6 +51,15 @@ public:
 template <typename T>
 T Vec2<T>::norm( ){
     return sqrt(*this * *this);
+}
+
+template <typename T>
+Vec2<T> Vec2<T>::normalized() {
+    Vec2<T> output;
+    T norm_val = this->norm();
+    output.x /= norm_val;
+    output.y /= norm_val;
+    return output;
 }
 
 // double vector
@@ -58,36 +76,7 @@ template <typename T>
 int sign(T val) {return val < 0;}
 
 // Returns indices of inflection points
-template <typename T>
-std::vector<std::size_t> find_inflections(std::vector<Vec2<T> > points, double threshold=10) {
-    std::size_t N = points.size();
-    if (N < 3) {
-        return std::vector<std::size_t>();
-    }
-    double first_angle = calc_angle(points[0] - points[N-1], Vec2d(1,0));
-    double second_angle = calc_angle(points[1] - points[0], Vec2d(1,0));
-    printf("raw: %f\nraw: %f\n", first_angle, second_angle);
-    int last_sign = sign(second_angle - first_angle);
-
-    double last_angle = second_angle;
-    std::vector<std::size_t> inflections;
-    for (std::size_t i = 0; i < N; ++i) {
-        // printf("x: %f, y: %f\n", points[i].x, points[i].y);
-        Vec2<T> new_vec = points[i] - points[i-1];
-        // double angle = calc_angle(new_vec, last_vec);
-        double raw_angle = calc_angle(new_vec, Vec2d(1,0));
-        double angle_diff = raw_angle - last_angle;
-        int this_sign = sign(angle_diff);
-        if (this_sign != last_sign) {
-            printf("Turning point at %f\n", points[i].x);
-            inflections.push_back(i);
-        }
-        printf("raw: %f, diff: %f\n", raw_angle, angle_diff);
-        last_angle = raw_angle;
-        last_sign = this_sign;
-    }
-    return inflections;
-};
+std::vector<std::size_t> find_inflections(std::vector<Vec2d > points, double threshold=10);
 
 
 cv::Mat draw_curve(const std::vector<Vec2d>& points, int width, std::vector<Vec2d> inflections);
