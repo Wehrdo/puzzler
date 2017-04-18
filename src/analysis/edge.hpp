4e633b92977796504e32b66e3974cb154aeedcb4
David Wehr <dawehr@iastate.edu>
2017-04-17 20:55:28 -0500

    Refactoring of edge.hpp and beginning work on Edge::compare

--- src/analysis/edge.hpp
+++ src/analysis/edge.hpp
@@ -1,7 +1,10 @@
 #ifndef EDGE_H
 #define EDGE_H
 
+#include <opencv2/opencv.hpp>
 #include "curve.hpp"
+#include "piece.hpp"
+#include <vector>
 
 class Edge
    {
@@ -11,62 +14,15 @@ class Edge
       std::vector<cv::Point> points;
       std::vector<Curve::curve_type> types;
 
-      Edge( void )
-         {
+      Edge( void );
+      Edge( Piece piece, std::vector<Curve> curves );
 
-         }
+      void translate( void );
 
-      Edge( Piece piece, std::vector<Curve> curves )
-         {
-         this->origin = curves[0].origin;
+      void rotate( cv::Point that );
 
-         if( curves.size() >= 1 )
-            this->handle = curves[curves.size()-1].origin;
-
-         this->points = std::vector<cv::Point>(
-            &(piece.contour[curves[0].start]),
-            &(piece.contour[curves[curves.size()-1].end]) );
-
-         for( Curve curve : curves )
-            this->types.push_back( curve.type );
-         }
-
-
-      void translate( void )
-         {
-         // Translate wrt origin
-         float delta_x = 0 - origin.x;
-         float delta_y = 0 - origin.y;
-         for( cv::Point pt : points )
-            {
-            pt.x += delta_x;
-            pt.y += delta_y;
-            }
-         handle.x += delta_x;
-         handle.y += delta_y;
-         }
-
-      void rotate( cv::Point that )
-         {
-         // Rotate wrt point and handle
-
-         float angle = this->handle.dot(that);
-
-         float len1 = sqrt(this->handle.x * this->handle.x + this->handle.y * this->handle.y);
-         float len2 = sqrt(that.x * that.x + that.y * that.y);
-
-         float a = this->handle.dot(that) / (len1 * len2);
-
-         angle = (a >= 0 ? 1.0 : -1.0) * acos(a);
-
-         cv::Mat transformation = cv::getRotationMatrix2D(this->origin,angle,1);
-         cv::transform(this->points,this->points,transformation);
-         }
-
-      float compare( Edge that )
-         {
-         // Compare with another edge
-         }
+      // Compare with another edge
+      float compare(const Edge& that );
 
    };
 
